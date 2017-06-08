00a8fa3a74249b1290fa61d6c734c292bd60bdd6
Delton Ding <dsh0416@gmail.com>
2017-06-08 10:36:42 +0800

    Add type check

--- lib/operators/arg.rb
+++ lib/operators/arg.rb
@@ -3,6 +3,7 @@ module Blockly::Operators
     attr_accessor :name, :value
 
     def initialize(name, value=nil)
+      raise TypeError.new('Arg name is not a string') unless name.is_a? String
       @name = name
       @value = value
     end
--- lib/operators/mutation.rb
+++ lib/operators/mutation.rb
@@ -4,6 +4,7 @@ module Blockly::Operators
 
     def initialize(args)
       @args = args.inject({}) do |hash, arg|
+        raise TypeError.new('Mutation only accepts Arg array as parameter') unless arg.is_a? Arg
         hash[arg.name.to_sym] = arg.value
       end
     end
